28b1a4f527a13cb53e700a1f062fa12f74ed3e32
AlexandreDjazz <alexandrehelleux@hotmail.fr>
2025-02-06 16:47:41 +0100

    Update index
    attention execute une fois sinon doublon antenne

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,27 @@
-import { generateFakeData } from './populate';
-import { mongoose, neo4jSession, neo4jDriver } from './connexion';
-
-
-
+import { generateFakeData } from "./populate";
+import { importAntennas } from "./importAntennas";
+import { mongoose, neo4jSession, neo4jDriver } from "./connexion";
 
 async function main() {
-    //console.log('Lancement du script...');
-    await generateFakeData();
+
+    try {
+        await generateFakeData();
+        console.log("Données factices insérées !");
+
+        await importAntennas();
+        console.log("Antennes relais importées !");
+    } catch (error) {
+        console.error("Erreur lors de l'exécution :", error);
+    }
 }
+main()
+    .then(() => console.log("Exécution complète"))
+    .catch(err => console.error("Erreur lors de l'exécution :", err));
 
-main().then(() => {
-   // console.log('Exécution terminée');
-}).catch(err => console.error('Erreur lors de l\'exécution', err));
 
-
-process.on('exit', async () => {
+process.on("exit", async () => {
     await mongoose.disconnect();
     await neo4jSession.close();
     await neo4jDriver.close();
-    console.log('Connexions fermées');
+    console.log("Connexions fermées");
 });
