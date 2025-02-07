552da440ae4a3055d959161538e9cff00c3ce521
Spatulox <M78stormtrooper@laposte.net>
2025-02-07 15:39:55 +0100

    fix the /search endpoint for case

--- src/routes/case.ts
+++ src/routes/case.ts
@@ -14,20 +14,64 @@ caseRoutes.get('/all', async (req: Request, res: Response) => {
     }
 });
 
-// /case/search?q=TitreDuCas
+// /case/search?q_title=TitreDuCas
+// /case/search?q_time=heure en 2025-02-08 ou 2025-02-08T10:00
+// /case/search?q_location=id or name
 caseRoutes.get('/search', async (req: Request, res: Response) => {
-    const query = req.query.q as string;
+    const query = req.query.q_title as string | undefined;
+    const timeString = req.query.q_time as string | undefined;
+    const location = req.query.q_location as string | undefined;
 
-    try{
-        const case_content = await Case.find({ title: new RegExp(query, 'i') }).populate("location")
-        if (!case_content || case_content.length == 0){
-            return exitWithMessage(res, "Cas non trouvé", 404);
+    let searchCriteria: any = {};
+
+    if (query) {
+        searchCriteria.title = new RegExp(query, 'i');
+    }
+
+    if (timeString) {
+        const date = new Date(timeString);
+        if (!isNaN(date.getTime())) {
+            const twoHoursBefore = new Date(date.getTime() - 2 * 60 * 60 * 1000);
+            const twoHoursAfter = new Date(date.getTime() + 2 * 60 * 60 * 1000);
+            searchCriteria.date = { 
+                $gte: twoHoursBefore,
+                $lte: twoHoursAfter
+            };
+        } else {
+            return exitWithMessage(res, "Format de date invalide", HttpStatus.BAD_REQUEST);
         }
-        exitWithContent(res, case_content)
-        return
-    } catch(err){
-        exitWithMessage(res, `${err}`)
-        return
+    }
+
+    // Location
+    if (location) {
+        if (isMongoId(location)) {
+            searchCriteria.location = ObjectId(location);
+        } else {
+            try {
+                const locationDoc = await Location.findOne({ address: new RegExp(location, 'i') }).select('_id');
+                if (locationDoc) {
+                    searchCriteria.location = locationDoc._id;
+                } else {
+                    return exitWithMessage(res, "Location non trouvée", HttpStatus.NOT_FOUND);
+                }
+            } catch (err) {
+                return exitWithMessage(res, `Erreur lors de la recherche de la location: ${err}`, HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+    }
+    console.log(searchCriteria)
+    try {
+        const case_content = await Case.find(searchCriteria)
+            .populate("location individuals")
+            .exec();
+
+        if (!case_content || case_content.length === 0) {
+            return exitWithMessage(res, "Aucun cas trouvé", HttpStatus.NOT_FOUND);
+        }
+
+        exitWithContent(res, case_content);
+    } catch (err) {
+        exitWithMessage(res, `Erreur lors de la recherche: ${err}`, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 });
 
