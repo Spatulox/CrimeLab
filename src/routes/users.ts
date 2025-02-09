fb0d619b3f01f481aebcd2cdea13c0918dac7569
Spatulox <M78stormtrooper@laposte.net>
2025-02-09 15:04:25 +0100

    now can search all user's case base on (ID is mandatory):
    their ID
    a location
    a date

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -2,18 +2,42 @@ import { Router, Request, Response } from 'express';
 import { exitWithMessage, exitWithContent, HttpStatus, isMongoId, ObjectId } from './shared'
 import { Individual, Case, Testimony, CallRecord ,Location,Antenna} from '../schema'
 import { Types } from 'mongoose'
+import {neo4jDriver} from "../connexion";
 
 export const userRoutes = Router();
 // /user/search?user=Richard
 userRoutes.get('/search', async (req: Request, res: Response) => {
     const user = req.query.user as string
-    try{
-        const user_content = await Individual.find({ name: new RegExp(user, 'i') })
-        exitWithContent(res, user_content)
-        return
-    } catch(err){
-        exitWithMessage(res, `Something went wrong : ${err}`)
-        return
+    const id = req.query.id as string
+
+    if(user) {
+        try{
+            const user_content = await Individual.find({ name: new RegExp(user, 'i') })
+            if(user_content.length == 0){
+                exitWithMessage(res, `No users`, HttpStatus.NOT_FOUND)
+                return
+            }
+            exitWithContent(res, user_content)
+            return
+        } catch(err){
+            exitWithMessage(res, `Something went wrong : ${err}`)
+            return
+        }
+    }
+
+    if(id && isMongoId(id)){
+        try{
+            const user_content = await Individual.findById(id)
+            if(!user_content){
+                exitWithMessage(res, `No users`, HttpStatus.NOT_FOUND)
+                return
+            }
+            exitWithContent(res, user_content)
+            return
+        } catch(err){
+            exitWithMessage(res, `Something went wrong : ${err}`)
+            return
+        }
     }
 });
 
@@ -21,6 +45,10 @@ userRoutes.get('/search', async (req: Request, res: Response) => {
 userRoutes.get('/all', async (req: Request, res: Response) => {
     try{
         const users = await Individual.find()
+        if(users.length == 0){
+            exitWithMessage(res, `No users`, HttpStatus.NOT_FOUND)
+            return
+        }
         exitWithContent(res, users)
         return
     } catch(err){
@@ -29,112 +57,88 @@ userRoutes.get('/all', async (req: Request, res: Response) => {
     }
 });
 
-// Get the user, if no time nor location
+// Get all the case where the user is impliqué
 // Get the case of the user based on their location and time
-// Ouais je sais, ca fait deux choses WELP
 userRoutes.get('/:id', async (req: Request, res: Response) => {
     try {
         const id = req.params.id;
         const timeString = req.query.time as string | undefined;
-        const location = req.query.location as string | "";
-        
+        const location = req.query.location as string | undefined;
+
         if (!isMongoId(id)) {
             exitWithMessage(res, "ID invalide", HttpStatus.BAD_REQUEST);
             return;
         }
-
-        const time = timeString ? new Date(timeString) : undefined;
-
-        // si on cherche que l'utilisateur
-        if (!time && !location) {
-            const user_content = await Individual.findById(id);
-            if (!user_content) {
-                exitWithMessage(res, "Utilisateur non trouvé", HttpStatus.NOT_FOUND);
-                return;
-            }
-            exitWithContent(res, user_content);
-            return;
-        }
-        
 
         const user = await Individual.findById(id);
         if (!user) {
             exitWithMessage(res, "Utilisateur non trouvé", HttpStatus.NOT_FOUND)
             return
         }
-        
-        let locationId: string = location;
 
-        if (location && !isMongoId(location)) {
-            const locationDoc = await Location.findOne({ address: location }).select('_id');
-            if (locationDoc) {
-                locationId = locationDoc._id.toString();
+        let query: any = { individuals: ObjectId(id) };
+
+        if (location) {
+            let locationId: string;
+            if (!isMongoId(location)) {
+                const locationDoc = await Location.findOne({ address: location }).select('_id');
+                if (locationDoc) {
+                    locationId = locationDoc._id.toString();
+                } else {
+                    exitWithMessage(res, "Location n'existe pas", HttpStatus.NOT_FOUND)
+                    return
+                }
             } else {
-                exitWithMessage(res, "Location don't exist", HttpStatus.NOT_FOUND)
-                return
+                locationId = location;
             }
+            query.location = ObjectId(locationId);
         }
 
-        let startTime: Date;
-        let endTime: Date;
-        let relevantCases
+        if (timeString) {
+            const time = new Date(timeString);
+            let startTime: Date, endTime: Date;
 
-        if(time){
-            if (timeString && !timeString.includes("T")) {
+            if (!timeString.includes("T")) {
                 startTime = new Date(time.setHours(0, 0, 0, 0));
                 endTime = new Date(time.setHours(23, 59, 59, 999));
             } else {
                 startTime = new Date(time.getTime() - 2 * 60 * 60 * 1000);
                 endTime = new Date(time.getTime() + 2 * 60 * 60 * 1000);
             }
-        
-            relevantCases = await Case.find({
-                individuals: ObjectId(id),
-                date: { $gte: startTime, $lte: endTime },
-                location: ObjectId(locationId)
-            }).populate('individuals location');
-        
-        } else {
-            relevantCases = await Case.find({
-                individuals: ObjectId(id),
-                location: ObjectId(locationId)
-            }).populate('individuals location');
+
+            query.date = { $gte: startTime, $lte: endTime };
         }
+
+        const relevantCases = await Case.find(query).populate('individuals location');
 
         const involvedIndividuals = Array.from(new Set(
             relevantCases.flatMap(c => c.individuals.map(i => i._id.toString()))
         ));
 
         const testimonies = await Testimony.find({
-            witness: { $in: involvedIndividuals.map(id => new Types.ObjectId(id)) },
+            witness: { $in: involvedIndividuals.map(id => ObjectId(id)) },
         }).populate("witness");
 
-        /*if(time){
-            const callRecords = await CallRecord.find({
-                $or: [{ caller: id }, { receiver: id }],
-                date: { 
-                    $gte: new Date(time.getTime() - 1000 * 60 * 60),
-                    $lte: new Date(time.getTime() + 1000 * 60 * 60)
-                }
-            }).populate('caller receiver');
-            
-            exitWithContent(res, {
-                user,
-                relevantCases,
-                testimonies,
-                callRecords
-            })
-            return
-        }*/
-
-        exitWithContent(res, {
+        let response: any = {
             user,
             relevantCases,
             testimonies
-        })
+        };
 
-        
-        return
+        if (timeString) {
+            const callRecords = await CallRecord.find({
+                $or: [
+                    { caller: { $in: involvedIndividuals.map(id => ObjectId(id)) } },
+                    { receiver: { $in: involvedIndividuals.map(id => ObjectId(id)) } }
+                ],
+                dateTime: query.date
+            }).populate('caller receiver antenna');
+
+            response.callRecords = callRecords;
+        }
+
+        exitWithContent(res, response);
+        return;
 
     } catch (error) {
         console.error(error);
@@ -142,7 +146,6 @@ userRoutes.get('/:id', async (req: Request, res: Response) => {
         return
     }
 });
-
 
 userRoutes.get('/?', (req: Request, res: Response) => {
     exitWithMessage(res, `Tu es perdu...`, HttpStatus.OK)
